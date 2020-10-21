c6e2d816a850c4cc7f7603eef1722555976c2542
miriamino <29210881+miriamino@users.noreply.github.com>
2020-10-21 15:09:23 +0200

    scraper functional

--- week04/helper_functions.py
+++ week04/helper_functions.py
@@ -3,36 +3,36 @@ import re
 from bs4 import BeautifulSoup
 import time
 
-# todo the?
+# todo "the" band names?
 def transform_name(string):
     result = string.lower().replace(" ", "-")
     return result
 
 
-def get_lyrics(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "lxml")
-    lyrics = []
-    verses = soup.find_all("p", class_="verse")
-    verses = map(soup.get_text, verses)
-    print(lyrics)
-    if lyrics:
-        artist = str(re.findall(r"lyrics-.*", url))
-        artist = re.sub(r"lyrics-", "", artist)
-        artist = re.sub(r".html", "", artist)
-        artist = re.sub(r"-", " ", artist)
-        artist = artist.title()
-        title = str(re.findall(r".+-lyrics", url))
-        title = re.sub(r"-lyrics", "", title)
-        title = re.sub(r"https:\/\/www\.metrolyrics\.com\/", "", title)
-        title = re.sub(r"-", " ", title)
-        title = title.title()
-        return [title, artist, lyrics]
+def get_lyricspages(artist):
+    pages = []
+    pgnum = 1
+    lyricspage0 = (
+        f"https://www.metrolyrics.com/{transform_name(artist)}-lyrics.html"
+    )
+    lyricspage1 = (
+        f"https://www.metrolyrics.com/{transform_name(artist)}-alpage-{pgnum}.html"
+    )
+    req0 = requests.get(lyricspage0)
+    req1 = requests.get(lyricspage1)
+    if req0.ok:
+        while req0.text != req1.text:
+            pages += [lyricspage1]
+            pgnum += 1
+            lyricspage1 = f"https://www.metrolyrics.com/{transform_name(artist)}-alpage-{pgnum}.html"
+            req1 = requests.get(lyricspage1)
+            time.sleep(3)           
+        return pages
     else:
-        pass
+        print(f"Ooops, can't find \"{artist}\", check spelling?")
 
-print(get_lyrics('https://www.metrolyrics.com/saggy-denim-lyrics-princess-nokia.html'))
 
+# todo check for doubles
 def get_urls(artist, lyricspages):
     urls = []
     for lyricspage in lyricspages:
@@ -48,25 +48,28 @@ def get_urls(artist, lyricspages):
     return urls
 
 
-def get_lyricspages(artist):
-    pages = []
-    pgnum = 1
-    lastpage = 0
-    lyricspage = (
-        f"https://www.metrolyrics.com/{transform_name(artist)}-alpage-{pgnum}.html"
-    )
-    if requests.get(lyricspage) != 404:
-        while lastpage == 0:
-            if re.match(r".*\d+", str(requests.get(lyricspage).url)) is not None:
-                pages += [lyricspage]
-                pgnum += 1
-                lyricspage = f"https://www.metrolyrics.com/{transform_name(artist)}-alpage-{pgnum}.html"
-                time.sleep(3)
-            else:
-                lastpage = 1
-        return pages
+def get_lyrics(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "lxml")
+    lyrics = []
+    verses = soup.find_all("p", class_="verse")
+    for v in verses:
+        verse = v.get_text()
+        verse = re.sub(r'\[.+\]', '', verse)
+        lyrics += [verse]
+    if lyrics:
+        artist = str(re.findall(r"lyrics-.*", url))
+        artist = re.sub(r"lyrics-", "", artist)
+        artist = re.sub(r".html", "", artist)
+        artist = re.sub(r"-", " ", artist)
+        artist = artist.title().strip("['']")
+        title = str(re.findall(r".+-lyrics", url))
+        title = re.sub(r"-lyrics", "", title)
+        title = re.sub(r"https:\/\/www\.metrolyrics\.com\/", "", title)
+        title = re.sub(r"-", " ", title)
+        title = title.title().strip("['']")
+        lyrics = '\n'.join(lyrics)
+        lyrics = lyrics.strip("[']")
+        return title, artist, lyrics
     else:
-        print(f"Ooops, can't find \"{artist}\", check spelling?")
-        return False
-
-# print(get_lyricspages('princess nokia'))
+        pass
--- week04/scrape-lyrics.py
+++ week04/scrape-lyrics.py
@@ -6,23 +6,18 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from helper_functions import get_urls, get_lyrics, transform_name, get_lyricspages
 
-# if not os.path.isfile('lyrics.csv'):
-lyrics_df = pd.DataFrame(columns=["title", "artist", "lyrics"])
-#     artists = []
-# else:
-#     lyrics_df = pd.read_csv('lyrics.csv')
-#     artists = lyrics_df['artist'].unique()
+if not os.path.isfile('lyrics.csv'):
+    lyrics_df = pd.DataFrame(columns=["title", "artist", "lyrics"])
+    artists = []
+else:
+    lyrics_df = pd.read_csv('lyrics.csv', index_col=0)
+    artists = lyrics_df['artist'].unique()
 
+new_artist = input('Add artist: ')
+new_artist = new_artist.title()
 
-# new_artist = input('Add artist: ')
-# if not transform_name(new_artist) in map(transform_name, artists):
-#     artists += [new_artist]
-# else:
-#     print('Artist already in database.')
-
-artists = ['princess nokia']
-
-for artist in artists:
+if not new_artist in artists:
+    artist = new_artist
     lyricspages =  get_lyricspages(artist)
     if lyricspages:
         urls = get_urls(transform_name(artist), lyricspages)
@@ -30,13 +25,15 @@ for artist in artists:
         for url in urls:
             data = get_lyrics(url)
             if data:
-                t = data[0].strip("[]")
-                a = data[1].strip("[]")
-                l = ' '.join(data[2])
+                t = data[0]
+                a = data[1]
+                l = data[2]
                 lyrics_df = lyrics_df.append({"title": t, "artist": a, "lyrics": l}, ignore_index=True)
                 counter +=1
             else:
                 pass
         print(f'Added {counter} songs by {artist}')
+    lyrics_df.to_csv('lyrics.csv')
+else:
+    print('Artist already in database.')
 
-lyrics_df.to_csv('lyrics.csv')
